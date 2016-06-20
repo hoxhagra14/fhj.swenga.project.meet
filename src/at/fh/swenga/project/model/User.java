4a1ac7370597b98142ff9a01bd2075fd9432263d
Wolfi <Wolfi@10.41.1.101>
2016-06-20 23:53:21 +0200

    further information about user

--- src/at/fh/swenga/project/model/User.java
+++ src/at/fh/swenga/project/model/User.java
@@ -1,14 +1,15 @@
 package at.fh.swenga.project.model;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -20,8 +21,20 @@ public class User implements java.io.Serializable {
 	private String username;
 	private String password;
 	private boolean enabled;
-	private int i;
+	@Column(name="name")
+	private String name;
+	@Column(name="age")
+	private int age;
+	@Column(name="city")
+	private String city;
+
 	private Set<UserRole> userRole = new HashSet<UserRole>(0);
+	
+//	@OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
+//	List<Activity> ownedActivities;
+	
+//	@ManyToMany(cascade = CascadeType.PERSIST, fetch=FetchType.EAGER)
+//	List<Activity> activities;
 
 	public User() {
 	}
@@ -40,7 +53,15 @@ public class User implements java.io.Serializable {
 		this.userRole = userRole;
 	}
 	
-	
+	public User(String username, String password, boolean enabled,
+			Set<UserRole> userRole, String name, int age, String city) {
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.name = name;
+		this.age = age;
+		this.city = city;
+	}
 	@Id
 	@Column(name = "username", unique = true, nullable = false, length = 45)
 	public String getUsername() {
@@ -77,4 +98,29 @@ public class User implements java.io.Serializable {
 	public void setUserRole(Set<UserRole> userRole) {
 		this.userRole = userRole;
 	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 }
