75782f83132211af607c33a0f9209ccd3244f1a2
Wolfi <Wolfi@10.41.1.176>
2016-06-22 16:34:12 +0200

    hash function for password

--- src/at/fh/swenga/project/model/User.java
+++ src/at/fh/swenga/project/model/User.java
@@ -161,39 +161,6 @@ public class User implements java.io.Serializable {
 		this.age = age;
 	}
 
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-	
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
 	public List<Activity> getOwnedActivities() {
 		return ownedActivities;
 	}
